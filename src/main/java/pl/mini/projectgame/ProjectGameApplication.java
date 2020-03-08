136b86503a76ba714059d528eedbe6b6649831d8
buensons <damian.roszczyk@gmail.com>
2020-03-08 22:38:46 +0100

    initial java commit

---
+++ src/main/java/pl/mini/projectgame/ProjectGameApplication.java
@@ -0,0 +1,13 @@
+package pl.mini.projectgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGameApplication.class, args);
+	}
+
+}
