16c008ad5e5711e05ee879e8d3a185f59ba93dca
buensons <damian.roszczyk@gmail.com>
2020-04-08 12:52:57 +0200

    add Board.java add BoardObject.java add Cell.java add Position.java add DeniedMoveException.java

---
+++ src/main/java/pl/mini/projectgame/models/Position.java
@@ -0,0 +1,31 @@
+package pl.mini.projectgame.models;
+
+/**
+ * @author buensons
+ */
+
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
