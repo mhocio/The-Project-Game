881462e857e19280462d834c16ac399236d6f38d
nowickam <nowicka444@gmail.com>
2020-04-13 19:29:56 +0200

    Add invalid team size exception and  null player check

---
+++ src/main/java/pl/mini/projectgame/exceptions/InvalidTeamSizeException.java
@@ -0,0 +1,7 @@
+package pl.mini.projectgame.exceptions;
+
+public class InvalidTeamSizeException extends Exception {
+    public InvalidTeamSizeException(String message){
+        super(message);
+    }
+}
--- src/main/java/pl/mini/projectgame/models/Team.java
+++ src/main/java/pl/mini/projectgame/models/Team.java
@@ -1,73 +1,83 @@
 package pl.mini.projectgame.models;
 
+import pl.mini.projectgame.exceptions.InvalidTeamSizeException;
+
 import java.util.Hashtable;
 import java.util.Map;
 
 public class Team {
-    public TeamColor teamColor;
+    private TeamColor teamColor;
     //public TeamRole teamRole;
-    public int size;
+    private int size;
     private Hashtable<Player, TeamRole> players;
 
-    public Team(){
-        players=new Hashtable<Player, TeamRole>();
-        size=0;
+    public Team() {
+        players = new Hashtable<Player, TeamRole>();
+        size = 0;
+        teamColor = null;
     }
 
-    private enum TeamColor{
-        RED,BLUE;
+    public enum TeamColor {
+        RED, BLUE;
     }
 
-    private enum TeamRole{
-        LEADER,MEMBER
+    public enum TeamRole {
+        LEADER, MEMBER
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public Hashtable getPlayers(){
+    public Hashtable getPlayers() {
         return players;
     }
 
-    public void addPlayer(Player player){
-        if(!players.containsKey(player))
-        {
-            players.put(player,null);
+    public void addPlayer(Player player)
+            throws InvalidTeamSizeException {
+        if (this.getSize() == 4)
+            throw new InvalidTeamSizeException("This team is full!");
+        if (player != null && !players.containsKey(player)) {
+            players.put(player, TeamRole.MEMBER);
             size++;
         }
     }
 
-    public void removePlayer(Player player){
-        if(!players.containsKey(player))
-        {
+    public void removePlayer(Player player)
+        throws InvalidTeamSizeException{
+        if (player != null && players.containsKey(player)) {
             players.remove(player);
             size--;
         }
+        if(this.getSize()==0)
+            throw new InvalidTeamSizeException("This team is empty!");
     }
 
-    public TeamColor getColor(){
+    public TeamColor getColor() {
         return teamColor;
     }
 
-    public void setColor(TeamColor color){
-        teamColor=color;
+    public void setColor(TeamColor color) {
+        teamColor = color;
     }
 
-    public TeamRole getRole(Player player){
+    public TeamRole getRole(Player player) {
         return players.get(player);
     }
 
-    public void setRole(Player player, TeamRole role){
-        if(players.containsKey(player))
-            players.replace(player,role);
+    public void setRole(Player player, TeamRole role) {
+        if (players.containsKey(player)) {
+            if (role == TeamRole.LEADER && this.getLeader() != null)
+                players.replace(this.getLeader(), TeamRole.MEMBER);
+            players.replace(player, role);
+        }
     }
 
     public Player getLeader() {
-        Player leader=null;
+        Player leader = null;
         for (Map.Entry entry : players.entrySet()) {
             if (TeamRole.LEADER.equals(entry.getValue())) {
-                leader =(Player)entry.getKey();
+                leader = (Player) entry.getKey();
                 break;
             }
         }
