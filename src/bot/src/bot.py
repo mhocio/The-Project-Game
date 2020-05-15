8d843ae13b54931f6c3da9a48508e4660df2141f
nowickam <nowicka444@gmail.com>
2020-05-15 16:53:54 +0200

    working bot

--- src/bot/src/bot.py
+++ src/bot/src/bot.py
@@ -14,7 +14,9 @@ def bot_function(addr):
     my_player.start()
     my_player.move_right()
     my_player.move_left()
+    my_player.finish()
     my_player.close()
+    print("END BOT FUNCTION")
 
 
 class Role(Enum):
@@ -41,6 +43,7 @@ class Board:
 
 class Player:
     writing = True
+    is_carrying_piece = False
 
     def __init__(self, _host = '127.0.0.1', _port = 8080):
         self.HOST = _host
@@ -51,17 +54,17 @@ class Player:
     def set_guid(self, guid):
         self.GUID = guid
 
-    def bot_read(self):
+    def reading_thread(self):
         while(True):
             rv = self.recv()
             print("RECV: ", rv)
             if(rv['action'] == "finish"):
                 print("BOT_READ finish")
                 break
-            elif rv['action'] == 'start' and rv['status'] == "OK":
-                print("BOT_READ start")
-                self.writing = False
-                pass
+            # elif rv['action'] == 'start' and rv['status'] == "OK":
+            #     print("BOT_READ start")
+            #     self.writing = False
+            #     pass
             # response for start message from host
             elif rv['action'] == 'discover' and rv['status'] == "OK":
                 print("BOT_READ discover")
@@ -70,12 +73,13 @@ class Player:
             elif rv['action'] == 'test' and rv['status'] == "OK":
                 # TODO test piece status update
                 pass
-            elif rv['action'] == 'move':
+            elif rv['action'] == 'move' and rv['status'] == "OK":
                 print("BOT_READ move")
-                if rv['status'] == "OK":
-                    print("BOT_READ position1 "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
-                    self.set_pos(rv['position']['x'], rv['position']['y'])
-                    print("BOT_READ position2 "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
+                print("BOT_READ position1 "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
+                self.set_pos(rv['position']['x'], rv['position']['y'])
+                print("BOT_READ position2 "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
+            elif rv['action'] == 'pickup' and rv['status'] == "OK":
+                self.is_carrying_piece = True
 
             self.writing = True
 
@@ -112,6 +116,14 @@ class Player:
             pass
         print("WRITING "+str(self.writing))
 
+    def place_piece(self):
+        self.wait()
+        TestMessage={
+            "action": "test",
+            "playerUuid": self.get_guid()
+            }
+        self.send(TestMessage)
+        self.writing = False
 
     def move_right(self):
         self.wait()
@@ -131,6 +143,7 @@ class Player:
 
     def move_left(self):
         self.wait()
+        print("POSITION before "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
         print("WRITING before "+str(self.writing))
         MoveMessage={
             "action": "move",
@@ -143,6 +156,7 @@ class Player:
         }
         self.send(MoveMessage)
         self.writing = False
+        print("POSITION after "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
         print("WRITING after "+str(self.writing))
 
 
@@ -214,12 +228,23 @@ class Player:
         self.send(MoveMessage)
         self.writing = False
 
+    def finish(self):
+        self.wait()
+        MoveMessage={
+            "action": "finish",
+            "playerUuid": self.get_guid(),
+        }
+        self.send(MoveMessage)
+        self.writing = False
+
     def connect(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.HOST, self.PORT))
     
     def close(self):
+        self.x.join()
         self.socket.close()
+        print("CLOSING SOCKET")
 
     def send(self, message):
         self.socket.sendall(bytes(json.JSONEncoder().encode(message), "utf-8"))
@@ -260,7 +285,7 @@ class Player:
                 self.set_pos(int(config["position"]["x"]), int(config["position"]["y"]))
 
                 if config['status'] == 'OK':
-                    self.x = Thread(target = self.bot_read)
+                    self.x = Thread(target = self.reading_thread)
                     self.x.start()
 
 
