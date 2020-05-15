36b734b9ff5d8a085605d63b7993435b03be3e05
Mikołaj Hościło <56316211+mhocio@users.noreply.github.com>
2020-05-15 14:37:30 +0200

    Fix add new piece by GM and fix constructor (#50)
    * Fix add new piece by GM and fix constructor
    
    * Change the random int generator

--- src/main/java/pl/mini/projectgame/GameMaster.java
+++ src/main/java/pl/mini/projectgame/GameMaster.java
@@ -17,6 +17,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 @Getter
@@ -62,6 +63,8 @@ public class GameMaster {
         masterBoard = board;
         blueTeam = new Team(Team.TeamColor.BLUE);
         redTeam = new Team(Team.TeamColor.RED);
+        blueTeamGoals = new ArrayList<>();
+        redTeamGoals = new ArrayList<>();
         pieces = new ArrayList<>();
         mode = gmMode.NONE;
 
@@ -186,17 +189,27 @@ public class GameMaster {
     private void putNewPiece() {
         var target = new Position();
 
-        Random random = new Random();
+        //Random random = new Random();
         var piece = new Piece(configuration.getShamProbability());
 
-        target.setY(random.nextInt() % masterBoard.getTaskAreaHeight() + masterBoard.getGoalAreaHeight());
-        target.setX(random.nextInt(masterBoard.getWidth()));
+        //target.setY(random.nextInt() % masterBoard.getTaskAreaHeight() + masterBoard.getGoalAreaHeight());
+        //target.setX(random.nextInt(masterBoard.getWidth()));
 
-        while (masterBoard.getCells().get(target).getContent().containsKey(Player.class)) {
-            target.setY(random.nextInt() % masterBoard.getTaskAreaHeight() + masterBoard.getGoalAreaHeight());
-            target.setX(random.nextInt(masterBoard.getWidth()));
+        target.setY(ThreadLocalRandom.current().nextInt(0, masterBoard.getTaskAreaHeight())
+                    + masterBoard.getGoalAreaHeight());
+        target.setX(ThreadLocalRandom.current().nextInt(0, masterBoard.getWidth()));
+
+        while (masterBoard.getCells().get(target).getContent().containsKey(Player.class)
+            || masterBoard.getCells().get(target).getContent().containsKey(Piece.class)) {
+            //target.setY(random.nextInt() % masterBoard.getTaskAreaHeight() + masterBoard.getGoalAreaHeight());
+            //target.setX(random.nextInt(masterBoard.getWidth()));
+            target.setY(ThreadLocalRandom.current().nextInt(0, masterBoard.getTaskAreaHeight())
+                    + masterBoard.getGoalAreaHeight());
+            target.setX(ThreadLocalRandom.current().nextInt(0, masterBoard.getWidth()));
         }
 
+        piece.setPosition(target);
+        pieces.add(piece);
         masterBoard.getCells().get(target).addContent(Piece.class, piece);
     }
 
