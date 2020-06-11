d71be124667928b03fd0c17978de6cc7f43ce3c1
Mikołaj <mikolajhoscilo@gmail.com>
2020-06-11 20:38:29 +0200

    Change ports

--- src/bot/src/run.py
+++ src/bot/src/run.py
@@ -2,7 +2,6 @@ import sys
 import bot as bot
 from threading import Thread
 
-# threads = list()
 # n = int(sys.argv[1])
 addr = '127.0.0.1'
 # addr = str(sys.argv[1])
@@ -17,17 +16,6 @@ First argument -> server local ip address\n
 e.g. python3 master_bot.py 127.0.0.1
 ---------------------------------------------------
 """)
-'''
-for index in range(n):
-    x = Thread(target = bot.bot_function, args = [addr])
-    threads.append(x)
-    x.start()
-
-for thread in threads:
-    thread.join()
-    print("END BOT") '''
-    
-# x = Thread(target = bot.bot_function, args = [addr, 8080])
 x = Thread(target = bot.bot_function, args = [addr])
 x.start()
 x.join()
