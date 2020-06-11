5309dad7e0d3029e8e7c4f09b078b24a67f8ca17
Filip Zawadka <filip.zawadka@hotmail.com>
2020-06-11 20:50:04 +0200

    not so smartbot

--- src/bot/src/smartbot.py
+++ src/bot/src/smartbot.py
@@ -1,48 +1,30 @@
 import socket
 import json
-import string
-import random
 from threading import get_ident
 from enum import Enum
 from threading import Thread
 from time import sleep 
 from random import randrange
 
-BUFFER_SIZE = 50000
+BUFFER_SIZE = 1024
 
 def bot_function(addr):
-    # print("I'm " + str(get_ident()))
-    # my_player = Player(_host=addr)
-    
-    # my_player.start()
-    # my_player.move_right()
-    # my_player.move_left()
-    # my_player.move_down()
-    # my_player.move_up()
-    # my_player.finish()
-    # my_player.close()
-    # print("END BOT FUNCTION")
-
-
     print("I'm " + str(get_ident()))
     my_player = Player(_host=addr)
     my_player.start()
 
     while(True):
         my_player.leaveGoalArea()
+        my_player.discoverAndTryToPickUpAll()
         if(my_player.is_carrying_piece):
             my_player.test()
             if(my_player.is_carrying_piece):
                 my_player.goAndPlacePiece()
-        else:
-            my_player.discoverAndTryToPickUpAll()
     
     my_player.finish()
     my_player.close()
+    
 
-def randomString(stringLength = 8):
-    letters = string.ascii_letters
-    return ''.join(random.choice(letters) for i in range(stringLength))
 
 class Role(Enum):
     LEADER: 1
@@ -61,25 +43,31 @@ class Board:
         self.board_height = y
         self.board_width = x
 
+    def get_board_width(self):
+        return self.board_width
+
+    def get_board_height(self):
+        return self.board_height
+
     def set_cell(self, x, y, d):
         self.cells[y][x] = d
     
     def get_cell(self, x, y):
-        return self.cells[y][x]
-
+        return self.cells[x][y]
+        
     def show(self):
         for r in self.cells:
             for c in r:
                 print(c,end = " ")
             print()
-        print('\n')
+        print()
 
 
 class Player:
     writing = True
     is_carrying_piece = False
 
-    def __init__(self, _host = '127.0.0.1', _port = 8080):
+    def __init__(self, _host = '127.0.0.1', _port = 8000):
         self.HOST = _host
         self.PORT = _port
         self.GUID = '0000'
@@ -93,7 +81,8 @@ class Player:
             rv = self.recv()
             rv = {k: v for k, v in rv.items() if v is not None}  # remove Nones from dict
             print("RECV: ", rv)
-            if(rv['action'] == "end"):
+            if(rv['action'] == "finish"):
+                print("BOT_READ finish")
                 break
             # elif rv['action'] == 'start' and rv['status'] == "OK":
             #     print("BOT_READ start")
@@ -101,15 +90,18 @@ class Player:
             #     pass
             # response for start message from host
             elif rv['action'] == 'discover' and rv['status'] == "OK":
+                print("BOT_READ discover")
                 for field in rv['fields']:
                     self.board.set_cell(field['x'], field['y'], field['cell']['distance'])
             elif rv['action'] == 'test' and rv['status'] == "OK":
                 # TODO test piece status update
                 pass
             elif rv['action'] == 'move':
+                print("BOT_READ position before: "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
                 if rv['status'] == "OK":
                     self.set_pos(rv['position']['x'], rv['position']['y'])
-                elif rv['status'] == "DENIED":
+                print("BOT_READ move")
+                print("BOT_READ position after: "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
             elif rv['action'] == 'pickup' and rv['status'] == "OK":
                 self.is_carrying_piece = True
 
@@ -146,22 +138,24 @@ class Player:
     def wait(self):
         while(self.writing == False):
             pass
+        print("WRITING "+str(self.writing))
 
     def place_piece(self):
         self.wait()
         TestMessage={
             "action": "test",
-            "playerGuid": self.get_guid()
+            "playerUuid": self.get_guid()
             }
         self.send(TestMessage)
         self.writing = False
 
     def move_right(self):
         self.wait()
+        print("WRITING before "+str(self.writing))
         MoveMessage={
             "action": "move",
-            "playerGuid": self.get_guid(),
-            "direction": "Right",
+            "playerUuid": self.get_guid(),
+            "direction": "RIGHT",
             "position" : {
                 "x" : self.get_pos_x(),
                 "y" : self.get_pos_y()
@@ -169,13 +163,16 @@ class Player:
         }
         self.send(MoveMessage)
         self.writing = False
+        print("WRITING after "+str(self.writing))
 
     def move_left(self):
         self.wait()
+        print("POSITION before "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
+        print("WRITING before "+str(self.writing))
         MoveMessage={
             "action": "move",
-            "playerGuid": self.get_guid(),
-            "direction": "Left",
+            "playerUuid": self.get_guid(),
+            "direction": "LEFT",
             "position" : {
                 "x" : self.get_pos_x(),
                 "y" : self.get_pos_y()
@@ -183,28 +180,17 @@ class Player:
         }
         self.send(MoveMessage)
         self.writing = False
+        print("POSITION after "+str(self.get_pos_x())+" "+str(self.get_pos_y()))
+        print("WRITING after "+str(self.writing))
 
 
-    def move_down(self):
-        self.wait()
-        MoveMessage={
-            "action": "move",
-            "playerGuid": self.get_guid(),
-            "direction": "Down",
-            "position" : {
-                "x" : self.get_pos_x(),
-                "y" : self.get_pos_y()
-            }
-        }
-        self.send(MoveMessage)
-        self.writing = False
-        
     def move_up(self):
         self.wait()
+        print("WRITING before "+str(self.writing))
         MoveMessage={
             "action": "move",
-            "playerGuid": self.get_guid(),
-            "direction": "Up",
+            "playerUuid": self.get_guid(),
+            "direction": "UP",
             "position" : {
                 "x" : self.get_pos_x(),
                 "y" : self.get_pos_y()
@@ -212,6 +198,23 @@ class Player:
         }
         self.send(MoveMessage)
         self.writing = False
+        print("WRITING after "+str(self.writing))
+        
+    def move_down(self):
+        self.wait()
+        print("WRITING before "+str(self.writing))
+        MoveMessage={
+            "action": "move",
+            "playerUuid": self.get_guid(),
+            "direction": "DOWN",
+            "position" : {
+                "x" : self.get_pos_x(),
+                "y" : self.get_pos_y()
+            }
+        }
+        self.send(MoveMessage)
+        self.writing = False
+        print("WRITING after "+str(self.writing))
 
     def move(self, x, y):
 
@@ -235,21 +238,19 @@ class Player:
             elif horizontal_mode and x_direction < 0:
                 self.move_left()
             elif not horizontal_mode and y_direction > 0:
-                self.move_down()
-            elif not horizontal_mode and y_direction < 0:
                 self.move_up()
+            elif not horizontal_mode and y_direction < 0:
+                self.move_down()
 
             # after_pos = (self.get_pos_x(), self.get_pos_y())
             # TODO: checking if someone blocked, now going like zigzag
             horizontal_mode = not horizontal_mode
 
-            self.board.show()
-
     def pickup(self):
         self.wait()
         MoveMessage={
-            "action": "pickup",
-            "playerGuid": self.get_guid(),
+            "action": "pickUp",
+            "playerUuid": self.get_guid(),
         }
         self.send(MoveMessage)
         self.writing = False
@@ -258,7 +259,7 @@ class Player:
         self.wait()
         MoveMessage={
             "action": "test",
-            "playerGuid": self.get_guid(),
+            "playerUuid": self.get_guid(),
         }
         self.send(MoveMessage)
         self.writing = False
@@ -267,7 +268,7 @@ class Player:
         self.wait()
         MoveMessage={
             "action": "place",
-            "playerGuid": self.get_guid(),
+            "playerUuid": self.get_guid(),
         }
         self.send(MoveMessage)
         self.writing = False
@@ -276,7 +277,7 @@ class Player:
         self.wait()
         MoveMessage={
             "action": "discover",
-            "playerGuid": self.get_guid(),
+            "playerUuid": self.get_guid(),
         }
         self.send(MoveMessage)
         self.writing = False
@@ -285,7 +286,7 @@ class Player:
         self.wait()
         MoveMessage={
             "action": "finish",
-            "playerGuid": self.get_guid(),
+            "playerUuid": self.get_guid(),
         }
         self.send(MoveMessage)
         self.writing = False
@@ -304,33 +305,38 @@ class Player:
         self.socket.sendall(bytes(json.JSONEncoder().encode(message), "utf-8"))
 
     def recv(self):
-        message_string = self.socket.recv(BUFFER_SIZE)
-        return json.loads(message_string)
+        return json.loads(self.socket.recv(BUFFER_SIZE))
 
     def init_config(self):
         print(self.get_guid())
         print(self.get_host())
-
-        message = {
+        
+        if self.get_host() == True:
+            message = {
                 "action" : "start",
-                "playerGuid": self.get_guid()
+                "playerUuid": self.get_guid()
             }
-        config = {"status": "DENIED"}     
+            self.send(message)
+            print("SEND")
+            print(message)
+
+        config = self.recv()
+        print("CONFIG")
+        print(config)
 
         # wait until all the players are ready
-        
-        while config["status"] != "OK":
+        while config["status"] == "DENIED":
             sleep(5)
-            '''if self.get_host() == True:
+            if self.get_host() == True:
                 self.send(message)
                 print("SEND")
-                print(message)'''
+                print(message)
             config = self.recv()
             print("CONFIG while")
             print(config)
 
-        if config["action"] == "start":
-                self.set_board(config["board"]["boardWidth"], config["board"]["taskAreaHeight"] + config["board"]["goalAreaHeight"], config["board"]["goalAreaHeight"])
+        if config["action"] == "startGame":
+                self.set_board(config["board"]["width"], config["board"]["taskAreaHeight"] + 2 * config["board"]["goalAreaHeight"], config["board"]["goalAreaHeight"])
                 self.set_pos(int(config["position"]["x"]), int(config["position"]["y"]))
 
                 if config['status'] == 'OK':
@@ -341,7 +347,6 @@ class Player:
     def start(self):
         message = {
             "action" : "connect",
-            "playerGuid" : randomString(),
         }
 
         self.send(message)
@@ -350,29 +355,9 @@ class Player:
         connected = self.recv()
         print("CONNECTED")
         print(connected)
-        
-        config = {"status": "DENIED"}     
-        # wait for start
-        
-        while config["status"] != "OK":
-            sleep(1)
-            config = self.recv()
-            print("startMessage while")
-            print(config)
-            
-        if config["action"] == "start":
-                self.set_board(config["board"]["boardWidth"], config["board"]["taskAreaHeight"] + 2 * config["board"]["goalAreaHeight"], config["board"]["goalAreaHeight"])
-                self.set_pos(int(config["position"]["x"]), int(config["position"]["y"]))
-                print(config["team"])
-                self.set_team(config["team"])
-                self.set_guid(config["playerGuid"])
 
-                if config['status'] == 'OK':
-                    self.x = Thread(target = self.reading_thread)
-                    self.x.start()
-
-        '''if "status" in connected and connected["status"] == "OK":
-            self.set_guid(connected["playerGuid"])
+        if "status" in connected and connected["status"] == "OK":
+            self.set_guid(connected["playerUuid"])
             self.set_role(connected["teamRole"])
             self.set_team(connected["teamColor"])
             self.set_host(connected["host"])
@@ -380,7 +365,7 @@ class Player:
             print(self.get_guid())
             message = {
                 "action" : "ready",
-                "playerGuid": self.get_guid(),
+                "playerUuid": self.get_guid(),
                 "status" : "YES"
             }
             
@@ -391,66 +376,48 @@ class Player:
             print(ready)
 
             if "status" in ready and ready["status"] == "OK":
-                self.init_config()'''
-
+                self.init_config()
+    
+    
     def discoverAndTryToPickUpAll(self):
         self.discover()
         px=self.get_pos_x()
         py=self.get_pos_y()
-        self.board.set_cell(px,py,int((self.board.get_cell(px,py-1)+self.board.get_cell(px,py+1))/2))
         self.board.show()
-        # minVal = self.board.get_cell(self.get_pos_x(),self.get_pos_y())
-        minVal = self.board.board_height * self.board.board_width
-        minx=self.get_pos_x()-1
-        miny=self.get_pos_y()-1
-        maxx=self.get_pos_x()+1
-        maxy=self.get_pos_y()+1
-        if(minx<0):
-            minx=0
-        if(miny<0):
-            miny=0
-        if(maxx>self.board.board_width-1):
-            maxx=self.board.board_width-1
-
-        if(maxy>self.board.board_height-1):
-            maxy=self.board.board_height-1
-
-        for ix in range(minx,maxx+1):
-            for iy in range(miny,maxy+1):
-                if self.board.get_cell(ix,iy)<minVal:
+        minVal = self.board.get_cell(self.get_pos_x(),self.get_pos_y())
+        for ix in range(self.get_pos_x()-1,self.get_pos_x()+1):
+            for iy in range(self.get_pos_y()-1,self.get_pos_y()+1):
+                if(self.board.get_cell(ix,iy)<minVal):
                     px=ix
                     py=iy
                     minVal=self.board.get_cell(ix,iy)
-        print("MINVAL:", minVal, " AT:", px, py)
-        discover_pos_x = self.get_pos_x()
-        discover_pos_y = self.get_pos_y()
-        self.move(px,py)
-        if((abs(discover_pos_x-px)+abs(discover_pos_y-py))<2):
-            print("H Move to: x:", px+(px-discover_pos_x)*minVal," y:",py+(py-discover_pos_y)*minVal)
-            self.move(px+(px-discover_pos_x)*minVal,py+(py-discover_pos_y)*minVal)
+        if((abs(self.get_pos_x()-px)+abs(self.get_pos_y()-py))<2):
+            self.move(self.get_pos_x()+(self.get_pos_x()-px)*minVal,self.get_pos_y()+(self.get_pos_y()-py)*minVal)
             self.pickup()
         else:
-            for i in range(0,minVal+1):
-                if(px+(px-discover_pos_x)*i<0 or px+(px-discover_pos_x)*i >= self.board.board_width 
-                or py+(py-discover_pos_y*(minVal-i))< 0 or py+(py-discover_pos_y)*(minVal-i) >= self.board.board_height):
+            for i in range(0,minVal):
+                if(self.get_pos_x+(self.get_pos_x-px)*i<0 or self.get_pos_x()+(self.get_pos_x()-px)*i > self.board.board_width 
+                or self.get_pos_y+(self.get_pos_y-py)*(minVal-i)< 0 or self.get_pos_y+(self.get_pos_y-py)*(minVal-i) > self.board.board_height):
                     continue
-                print("D Move to: x:",px+(px-discover_pos_x)*i,"y:",py+(py-discover_pos_y)*(minVal-i))
-                self.move(px+(px-discover_pos_x)*i, py+(py-discover_pos_y)*(minVal-i))
+                self.move(self.get_pos_x()+(self.get_pos_x()-px)*i,self.get_pos_y+(self.get_pos_y-py)*(minVal-i))
                 self.pickup()
                 if(self.is_carrying_piece):
                     break
         
     def leaveGoalArea(self):
         while(self.get_pos_y()<self.board.goal_area_height):
-            self.move_down()
-
-        while(self.get_pos_y()>=(self.board.board_height-self.board.goal_area_height)):
             self.move_up()
+
+        while(self.get_pos_y()>=self.board.board_height-self.board.goal_area_height):
+            self.move_down()
 
     def goAndPlacePiece(self):
         if self.team == "RED":
-            self.move(randrange(self.board.board_width),self.board.board_height-self.board.goal_area_height+randrange(self.board.goal_area_height))
-            self.place()
-        else:
             self.move(randrange(self.board.board_width),randrange(self.board.goal_area_height))
             self.place()
+        else:
+            self.move(randrange(self.board.board_width),self.board.board_height-self.board.goal_area_height+randrange(self.board.goal_area_height))
+            self.place()
+
+
+
---
+++ src/bot/src/test.py
@@ -0,0 +1,3 @@
+from random import randrange
+for i in range(0,10):
+    print(randrange(2))
