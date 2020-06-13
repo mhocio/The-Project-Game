2ca915fce19e10542a12dc40111fbae835d528da
Mikołaj Hościło <56316211+mhocio@users.noreply.github.com>
2020-06-13 23:47:47 +0200

    Add generating goal_area_positions of other players to help them (#71)

--- src/bot/src/run.py
+++ src/bot/src/run.py
@@ -2,21 +2,21 @@ import sys
 import bot as bot
 from threading import Thread
 
-# n = int(sys.argv[1])
+# addr = '20.39.48.176'
 addr = '127.0.0.1'
-# addr = str(sys.argv[1])
+player_num = int(sys.argv[1])
 
 print("""
 ===================================================
-PYTHON PROJECT GAME BOT GENERATOR
+PYTHON PROJECT GAME BOT
 ===================================================
 HOW TO USE
 ---------------------------------------------------
-First argument -> server local ip address\n
-e.g. python3 master_bot.py 127.0.0.1
+First argument -> number of the bot in a team (starting from zero)\n
+e.g. python3 master_bot.py 
 ---------------------------------------------------
 """)
-x = Thread(target = bot.bot_function, args = [addr])
+x = Thread(target = bot.bot_function, args = [addr, player_num])
 x.start()
 x.join()
 print("END BOT")
